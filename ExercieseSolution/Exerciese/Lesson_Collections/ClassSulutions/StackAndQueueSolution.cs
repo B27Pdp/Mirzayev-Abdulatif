b930bcb4fb87a24a32112df25fb6b2db1939688c
MirzayevDeveloper <MirzayevDeveloper1@gamil.com>
2023-01-26 17:19:28 +0500

    Updated

---
+++ ExercieseSolution/Exerciese/Lesson_Collections/ClassSulutions/StackAndQueueSolution.cs
@@ -0,0 +1,36 @@
+﻿using Exerciese.Collections.ClassSulutions.classes;
+
+namespace Exerciese.Collections.ClassSulutions
+{
+    public class StackAndQueueSolutions
+    {
+        public static void StackRun(Page p)
+        {
+            Stack<Page> pages = new();
+            Page l = new Page() { Id = 2, Name = "Hello" };
+            pages.Push(p);
+            pages.Push(l);
+            pages.Push(new Page() { Id = 3, Name = "Salom" });
+
+            Console.WriteLine(pages.First().Name);
+
+            Console.WriteLine(pages.Peek().Name);
+
+            Console.WriteLine(pages.EnsureCapacity(7));
+            Console.WriteLine(pages);
+            pages.Push(new Page() { Id = 4, Name = "OOP" });
+            pages.Clear();
+
+            Console.WriteLine(pages.TryPop(out p));
+
+            Console.WriteLine();
+
+        }
+        public void QueueRun()
+        {
+            Queue<User> users = new();
+
+        }
+    }
+}
+
---
+++ ExercieseSolution/Exerciese/Lesson_Hashtable/LessonHashtable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exerciese.Lesson_Hashtable
+{
+    public class LessonHashtable
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
